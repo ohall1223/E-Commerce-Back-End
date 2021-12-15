09e6499ba4e594f2322a56788e262a707fc62525
Olivia Hall <ohall1223@gmail.com>
2021-12-14 19:30:27 -0700

    routes and models built but not tested

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      prinaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataType.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataType.INTEGER,
+      reference: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
